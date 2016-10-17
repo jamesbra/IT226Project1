36261239dc3aeaa850220f6c46e8fb5f4eaf0875
James, Brandon <bmjame1@ilstu.edu>
2016-10-17 00:06:43 -0500

    Methods started in DataIntegration

--- src/edu/it226/isu/DataIntegration.java
+++ src/edu/it226/isu/DataIntegration.java
@@ -1,6 +1,27 @@
 package edu.it226.isu;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class DataIntegration
 {
-
+	private ArrayList<Student> studentList;
+	private StringTokenizer stk;
+	private Scanner readFile;
+	private File myFile;
+	
+	public void compileData(String fileName){
+		
+	}
+	
+	public void outputFile(String userID, String fileName){
+		
+	}
+	public int[] gradeSearch(String course, String semester, String year){
+		return null;
+		
+	}
+	
 }
--- src/edu/it226/isu/Student.java
+++ src/edu/it226/isu/Student.java
@@ -1,6 +1,8 @@
 package edu.it226.isu;
 
+import java.util.ArrayList;
+
 public class Student
 {
-
+	private ArrayList<CourseData> coursesTaken;
 }
